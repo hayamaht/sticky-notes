894c6e5813da01c5aa9e76501ae05066cdcaf09d
hayama <hayamaroom@gmail.com>
2023-07-09 16:20:32 +0800

    Add Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimengModule } from './shared/primeng/primeng.module';
+import { enviroment } from 'src/envs/environment';
 
 @NgModule({
   declarations: [
@@ -15,6 +19,11 @@ import { PrimengModule } from './shared/primeng/primeng.module';
     BrowserAnimationsModule,
     AppRoutingModule,
     PrimengModule,
+
+    provideFirebaseApp(() => initializeApp(enviroment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/envs/environments.prod.ts
@@ -0,0 +1,12 @@
+export const enviroment = {
+  prodution: true,
+  firebase: {
+    apiKey: "AIzaSyBp2O60Ffgd9-iOYerRoIpLTp354yKASzo",
+    authDomain: "ht-sticky-notes.firebaseapp.com",
+    projectId: "ht-sticky-notes",
+    storageBucket: "ht-sticky-notes.appspot.com",
+    messagingSenderId: "613189213031",
+    appId: "1:613189213031:web:c3c14dd7cc342c7742adc2",
+    measurementId: "G-GEYW79C66L"
+  }
+}
